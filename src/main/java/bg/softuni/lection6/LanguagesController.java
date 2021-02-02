fae5bb9b4d22477e58fc507b2567a1992f29ca75
Gosho <georgi_tsenkov@abv.bg>
2021-02-02 10:48:26 +0200

    Initial commit

---
+++ src/main/java/bg/softuni/lection6/LanguagesController.java
@@ -0,0 +1,37 @@
+package bg.softuni.lection6;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@Controller
+public class LanguagesController {
+
+    private final String defaultLang = "bg";
+    private final List<String> allLangs = List.of("en", "bg", "de");
+
+    @PostMapping("/save")
+    public String save(@RequestParam String lang,
+                       HttpServletResponse response) {
+        Cookie cookie = new Cookie("langCookie", lang);
+        response.addCookie(cookie);
+
+        return "redirect:/all";
+
+    }
+
+    @GetMapping("/all")
+    public String allLangs(Model model,
+        @CookieValue(value = "langCookie", required = false, defaultValue = defaultLang) String lang) {
+        model.addAttribute("all", allLangs);
+        model.addAttribute("preferredLang", lang);
+        return "languages";
+    }
+}
---
+++ src/main/java/bg/softuni/lection6/Lection6Application.java
@@ -0,0 +1,15 @@
+package bg.softuni.lection6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lection6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lection6Application.class, args);
+    }
+
+
+}
+
